8deca10d4f13267da3e36c88a0f979c35fe9eb7a
Ester Viana Verza <ester.av@hotmail.com>
2022-10-18 19:45:12 -0300

    alteração req 2

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -5,14 +5,3 @@ export const loginEmail = (state) => ({
   type: LOGIN_EMAIL,
   email: state,
 });
-
-export function fetchAPI() {
-  const url = 'https://economia.awesomeapi.com.br/json/all';
-  return async (dispatch) => {
-    dispatch(requestAPI());
-    return fetch(url)
-      .then((response) => response.json())
-      .then((json) => dispatch(getAPI(json)))
-      .catch((error) => errorAPI(error));
-  };
-}
